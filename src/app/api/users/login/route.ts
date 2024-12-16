1a63c3afa5683d3d995fca32409b938b540094e8
Anubhav <anubhavsarangi517@gmail.com>
2024-12-16 22:03:58 +0530

    first commit

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,8 +1,8 @@
 import {connect}  from  "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
-import { NextRequest,NextResponse,  } from "next/server";
+import { NextRequest,NextResponse  } from "next/server";
 import bcryptjs from "bcryptjs";
-import { error } from "console";
+
 import jwt from "jsonwebtoken"
 
 
